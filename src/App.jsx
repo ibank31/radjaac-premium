69b9a97010d15ac88eb4d65dc25e06341eab148f
ibank31 <iqbalteguh01@gmail.com>
2026-05-07 21:17:58 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -457,27 +457,6 @@ export default function App() {
         </section>
 
 
-      {/* FLOATING BUTTON */}
-      <motion.a
-        href="https://wa.me/62882008246099"
-        target="_blank"
-        animate={{
-          scale: [1, 1.08, 1],
-        }}
-        transition={{
-          repeat: Infinity,
-          duration: 2,
-        }}
-        className="fixed bottom-7 right-7 z-50 w-16 h-16 rounded-full bg-green-500 flex items-center justify-center shadow-[0_0_40px_rgba(34,197,94,0.7)]"
-      >
-
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
-          alt="WhatsApp"
-          className="w-8 h-8"
-        />
-
-      </motion.a>
       {/* CTA CLOSING */}
       <section className="px-6 py-20">
         <div className="max-w-6xl mx-auto">
