bd325dd401b7d69935970aa20baef4e30e0ac28b
ibank31 <iqbalteguh01@gmail.com>
2026-05-08 20:36:25 +0700

    Add automatic sitemap generation for SEO

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://www.radjaac.com',
+      dynamicRoutes: [
+        '/service-ac-purwokerto',
+        '/cuci-ac-purwokerto',
+        '/instalasi-ac-banyumas'
+      ],
+      changefreq: 'weekly',
+      priority: {
+        '/': 1.0,
+        '/service-ac-purwokerto': 0.8,
+        '/cuci-ac-purwokerto': 0.8,
+        '/instalasi-ac-banyumas': 0.8
+      }
+    })
   ],
 })
