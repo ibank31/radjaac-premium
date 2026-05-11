0d7ae06e190986f14f6ba3a7a4fa0a195b3464ae
syuliastuti214-dotcom <syuliastuti214@gmail.com>
2026-05-11 14:49:07 +0700

    Route brand Daikin page

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,10 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
 
+          <Route
+            path="/brand/daikin"
+            element={<DaikinPurwokerto />}
+          />
 
           <Route
             path="/daikin-purwokerto"
