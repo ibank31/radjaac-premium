2577e0673196b66835211435f8a1b29a5a1b43f9
syuliastuti214-dotcom <syuliastuti214@gmail.com>
2026-05-12 22:26:49 +0700

    Add homepage category links

--- src/data/home/categoryItems.js
+++ src/data/home/categoryItems.js
@@ -5,20 +5,24 @@ export const CATEGORY_ITEMS = [
     title: "AC Rumah",
     desc: "Nyaman untuk hunian",
     Icon: House,
+    href: "/katalog/ac-split-rumah",
   },
   {
     title: "AC Inverter",
     desc: "Hemat listrik hingga 60%",
     Icon: Zap,
+    href: "/katalog",
   },
   {
     title: "Low Watt",
     desc: "Ideal untuk kos & kontrakan",
     Icon: Wind,
+    href: "/katalog",
   },
   {
     title: "AC Kantor",
     desc: "Solusi ruang komersial",
     Icon: Building2,
+    href: "/katalog",
   },
 ];
