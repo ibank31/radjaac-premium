1dc14dcc6a600221f5957685719db32fc02c465d
syuliastuti214-dotcom <syuliastuti214@gmail.com>
2026-05-14 16:39:39 +0700

    Combine blog post data sources

---
+++ src/data/allBlogPosts.js
@@ -0,0 +1,11 @@
+import { BLOG_POSTS as CORE_BLOG_POSTS } from "./blogPosts"
+import { INSTALLATION_BLOG_POST } from "./installationBlogPost"
+
+export const BLOG_POSTS = [
+  ...CORE_BLOG_POSTS,
+  INSTALLATION_BLOG_POST,
+]
+
+export function getBlogPostBySlug(slug) {
+  return BLOG_POSTS.find((post) => post.slug === slug)
+}
