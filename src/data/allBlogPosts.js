eb3a11a765321c53cc7b3486f03703b314a437bb
syuliastuti214-dotcom <syuliastuti214@gmail.com>
2026-05-14 17:47:19 +0700

    Sort articles by newest update date

--- src/data/allBlogPosts.js
+++ src/data/allBlogPosts.js
@@ -1,10 +1,15 @@
 import { BLOG_POSTS as CORE_BLOG_POSTS } from "./blogPosts.js"
 import { INSTALLATION_BLOG_POST } from "./installationBlogPost.js"
 
-export const BLOG_POSTS = [
+const ALL_BLOG_POSTS = [
   ...CORE_BLOG_POSTS,
   INSTALLATION_BLOG_POST,
 ]
+
+export const BLOG_POSTS = [...ALL_BLOG_POSTS].sort(
+  (firstPost, secondPost) =>
+    new Date(secondPost.updatedAt).getTime() - new Date(firstPost.updatedAt).getTime()
+)
 
 export function getBlogPostBySlug(slug) {
   return BLOG_POSTS.find((post) => post.slug === slug)
