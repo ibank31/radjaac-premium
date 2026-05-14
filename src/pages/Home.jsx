0d8adc7f9d081846f74b3c35c8367fdc3648ff12
syuliastuti214-dotcom <syuliastuti214@gmail.com>
2026-05-14 10:52:11 +0700

    Move homepage trust section below hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -94,11 +94,11 @@ export default function Home() {
       <div className="max-w-7xl mx-auto px-6 lg:px-8 pt-20">
         <HeroSection />
 
+        <TrustSection />
+
         <SalesLandingSection />
 
         <CategorySection />
-
-        <TrustSection />
 
         <BrandsSection />
 
