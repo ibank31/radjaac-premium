8c9ef3e039d890cc49db4cd6f9e22224f1e4aa47
syuliastuti214-dotcom <syuliastuti214@gmail.com>
2026-05-15 13:57:43 +0700

    Register kost AC article

--- src/data/allBlogPosts.js
+++ src/data/allBlogPosts.js
@@ -1,10 +1,12 @@
 import { BLOG_POSTS as CORE_BLOG_POSTS } from "./blogPosts.js"
 import { BANYUMAS_BLOG_POST } from "./banyumasBlogPost.js"
 import { INSTALLATION_BLOG_POST } from "./installationBlogPost.js"
+import { KOST_BLOG_POST } from "./kostBlogPost.js"
 import { LOW_WATT_ELECTRICITY_BLOG_POST } from "./lowWattElectricityBlogPost.js"
 
 const ALL_BLOG_POSTS = [
   ...CORE_BLOG_POSTS,
+  KOST_BLOG_POST,
   BANYUMAS_BLOG_POST,
   LOW_WATT_ELECTRICITY_BLOG_POST,
   INSTALLATION_BLOG_POST,
