580c171b2eaec88901442b83c3cf2a542b2e4faf
syuliastuti214-dotcom <syuliastuti214@gmail.com>
2026-05-16 17:08:50 +0700

    Register freon education blog post

--- src/data/allBlogPosts.js
+++ src/data/allBlogPosts.js
@@ -1,11 +1,13 @@
 import { BLOG_POSTS as CORE_BLOG_POSTS } from "./blogPosts.js"
 import { BANYUMAS_BLOG_POST } from "./banyumasBlogPost.js"
+import { FREON_EDUCATION_BLOG_POST } from "./freonEducationBlogPost.js"
 import { INSTALLATION_BLOG_POST } from "./installationBlogPost.js"
 import { KOST_BLOG_POST } from "./kostBlogPost.js"
 import { LOW_WATT_ELECTRICITY_BLOG_POST } from "./lowWattElectricityBlogPost.js"
 
 const ALL_BLOG_POSTS = [
   ...CORE_BLOG_POSTS,
+  FREON_EDUCATION_BLOG_POST,
   KOST_BLOG_POST,
   BANYUMAS_BLOG_POST,
   LOW_WATT_ELECTRICITY_BLOG_POST,
