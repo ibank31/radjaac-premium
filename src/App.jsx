69445c1860d28626bb9a043dc6a3d7e426ab2eba
syuliastuti214-dotcom <syuliastuti214@gmail.com>
2026-05-20 23:19:00 +0700

    Add Samsung brand route

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ const HisensePurwokerto = lazy(() => import("./pages/HisensePurwokerto"))
 const SansuiPurwokerto = lazy(() => import("./pages/SansuiPurwokerto"))
 const BrandLainnya = lazy(() => import("./pages/BrandLainnya"))
 const SharpPurwokerto = lazy(() => import("./pages/SharpPurwokerto"))
+const SamsungPurwokerto = lazy(() => import("./pages/SamsungPurwokerto"))
 const PanasonicPurwokerto = lazy(() => import("./pages/PanasonicPurwokerto"))
 const Blog = lazy(() => import("./pages/Blog"))
 const BlogPost = lazy(() => import("./pages/BlogPost"))
@@ -80,184 +81,56 @@ export default function App() {
         <Suspense fallback={null}>
           <Routes>
             <Route path="/" element={<Home />} />
-
             <Route path="/toko-ac-terdekat-purwokerto" element={<TokoAcTerdekatPurwokerto />} />
-
             <Route path="/toko-ac-terdekat" element={<Navigate to="/toko-ac-terdekat-purwokerto" replace />} />
-
             <Route path="/jual-ac-purwokerto" element={<JualAcPurwokerto />} />
-
             <Route path="/jual-ac-banyumas" element={<JualAcBanyumas />} />
-
             <Route path="/jual-ac-sokaraja" element={<JualAcSokaraja />} />
-
             <Route path="/jual-ac-ajibarang" element={<JualAcAjibarang />} />
-
             <Route path="/jual-ac-wangon" element={<JualAcWangon />} />
-
             <Route path="/jual-ac-cilongok" element={<JualAcCilongok />} />
-
             <Route path="/jual-ac-jatilawang" element={<JualAcJatilawang />} />
-
             <Route path="/jual-ac-purbalingga" element={<JualAcPurbalingga />} />
-
             <Route path="/jual-ac-banjarnegara" element={<JualAcBanjarnegara />} />
-
             <Route path="/jual-ac-cilacap" element={<JualAcCilacap />} />
-
             <Route path="/jual-ac-kebumen" element={<JualAcKebumen />} />
-
             <Route path="/jual-ac-tegal" element={<JualAcTegal />} />
-
             <Route path="/jual-ac-yogyakarta" element={<JualAcYogyakarta />} />
-
             <Route path="/katalog" element={<KatalogAc />} />
-
             <Route path="/kalkulator-pk-ac" element={<KalkulatorAc />} />
-
             <Route path="/pengadaan-ac-proyek" element={<PengadaanAcProyek />} />
-
-            <Route
-              path="/brand/daikin"
-              element={<DaikinPurwokerto />}
-            />
-
-            <Route
-              path="/daikin-purwokerto"
-              element={<Navigate to="/brand/daikin" replace />}
-            />
-
-            <Route
-              path="/brand/gree"
-              element={<GreePurwokerto />}
-            />
-
-            <Route
-              path="/gree-purwokerto"
-              element={<Navigate to="/brand/gree" replace />}
-            />
-
-            <Route
-              path="/brand/midea"
-              element={<MideaPurwokerto />}
-            />
-
-            <Route
-              path="/midea-purwokerto"
-              element={<Navigate to="/brand/midea" replace />}
-            />
-
-            <Route
-              path="/brand/hisense"
-              element={<HisensePurwokerto />}
-            />
-
-            <Route
-              path="/hisense-purwokerto"
-              element={<Navigate to="/brand/hisense" replace />}
-            />
-
-            <Route
-              path="/brand/sansui"
-              element={<SansuiPurwokerto />}
-            />
-
-            <Route
-              path="/sansui-purwokerto"
-              element={<Navigate to="/brand/sansui" replace />}
-            />
-
-            <Route
-              path="/brand/sharp"
-              element={<SharpPurwokerto />}
-            />
-
-            <Route
-              path="/sharp-purwokerto"
-              element={<Navigate to="/brand/sharp" replace />}
-            />
-
-            <Route
-              path="/brand/panasonic"
-              element={<PanasonicPurwokerto />}
-            />
-
-            <Route
-              path="/panasonic-purwokerto"
-              element={<Navigate to="/brand/panasonic" replace />}
-            />
-
-            <Route
-              path="/brand/lainnya"
-              element={<BrandLainnya />}
-            />
-
-            <Route
-              path="/ac-inverter-purwokerto"
-              element={<Navigate to="/katalog/ac-inverter" replace />}
-            />
-
-            <Route
-              path="/ac-low-watt-purwokerto"
-              element={<Navigate to="/katalog/ac-low-watt" replace />}
-            />
-
-            <Route
-              path="/ac-purwokerto-selatan"
-              element={<Navigate to="/jual-ac-purwokerto" replace />}
-            />
-
-            <Route
-              path="/instalasi-ac-banyumas"
-              element={<Navigate to="/jual-ac-banyumas" replace />}
-            />
-
-            <Route
-              path="/instalasi-profesional"
-              element={<InstalasiProfesional />}
-            />
-
-            <Route
-              path="/katalog/ac-split-rumah"
-              element={<AcSplitRumah />}
-            />
-
-            <Route
-              path="/katalog/ac-inverter"
-              element={<AcInverter />}
-            />
-
-            <Route
-              path="/katalog/ac-low-watt"
-              element={<AcLowWatt />}
-            />
-
-            <Route
-              path="/katalog/ac-kantor-komersial"
-              element={<AcKantorKomersial />}
-            />
-
+            <Route path="/brand/daikin" element={<DaikinPurwokerto />} />
+            <Route path="/daikin-purwokerto" element={<Navigate to="/brand/daikin" replace />} />
+            <Route path="/brand/gree" element={<GreePurwokerto />} />
+            <Route path="/gree-purwokerto" element={<Navigate to="/brand/gree" replace />} />
+            <Route path="/brand/midea" element={<MideaPurwokerto />} />
+            <Route path="/midea-purwokerto" element={<Navigate to="/brand/midea" replace />} />
+            <Route path="/brand/hisense" element={<HisensePurwokerto />} />
+            <Route path="/hisense-purwokerto" element={<Navigate to="/brand/hisense" replace />} />
+            <Route path="/brand/sansui" element={<SansuiPurwokerto />} />
+            <Route path="/sansui-purwokerto" element={<Navigate to="/brand/sansui" replace />} />
+            <Route path="/brand/sharp" element={<SharpPurwokerto />} />
+            <Route path="/sharp-purwokerto" element={<Navigate to="/brand/sharp" replace />} />
+            <Route path="/brand/samsung" element={<SamsungPurwokerto />} />
+            <Route path="/samsung-purwokerto" element={<Navigate to="/brand/samsung" replace />} />
+            <Route path="/brand/panasonic" element={<PanasonicPurwokerto />} />
+            <Route path="/panasonic-purwokerto" element={<Navigate to="/brand/panasonic" replace />} />
+            <Route path="/brand/lainnya" element={<BrandLainnya />} />
+            <Route path="/ac-inverter-purwokerto" element={<Navigate to="/katalog/ac-inverter" replace />} />
+            <Route path="/ac-low-watt-purwokerto" element={<Navigate to="/katalog/ac-low-watt" replace />} />
+            <Route path="/ac-purwokerto-selatan" element={<Navigate to="/jual-ac-purwokerto" replace />} />
+            <Route path="/instalasi-ac-banyumas" element={<Navigate to="/jual-ac-banyumas" replace />} />
+            <Route path="/instalasi-profesional" element={<InstalasiProfesional />} />
+            <Route path="/katalog/ac-split-rumah" element={<AcSplitRumah />} />
+            <Route path="/katalog/ac-inverter" element={<AcInverter />} />
+            <Route path="/katalog/ac-low-watt" element={<AcLowWatt />} />
+            <Route path="/katalog/ac-kantor-komersial" element={<AcKantorKomersial />} />
             <Route path="/artikel" element={<Blog />} />
-
             <Route path="/artikel/:slug" element={<BlogPost />} />
-
             <Route path="/gallery" element={<Gallery />} />
-
-            <Route
-              path="/tentang-kami"
-              element={<TentangKami />}
-            />
-
-            <Route
-              path="/kontak"
-              element={<Kontak />}
-            />
-
-            <Route
-              path="/portfolio"
-              element={<Navigate to="/gallery" replace />}
-            />
-
+            <Route path="/tentang-kami" element={<TentangKami />} />
+            <Route path="/kontak" element={<Kontak />} />
+            <Route path="/portfolio" element={<Navigate to="/gallery" replace />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Suspense>
